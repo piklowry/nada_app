ee8f8b83abc0896a484d6d879ffe50cae0c90952
patrick <piklowry05@gmail.com>
2014-04-09 09:30:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NadaApp::Application.config.secret_key_base = '29da0539f67bffc15726d18cf378f3f4e685b835327797fbb496cdd4a1434bc81a7161e684f114dbf3b3ecb54140de9d7850f747bf01d6dbb830bbe923271449'
